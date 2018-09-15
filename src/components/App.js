62ce81bc8ed8a3bbb712dbe5856259b220fb9797
Marta <martaguilar.soto@gmail.com>
2018-09-15 11:44:18 +0200

    fetch server API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mobiles: []
+    };
+  }
+
+  componentDidMount() {
+    var url = 'http://localhost:8001/phone';
+
+    fetch(url)
+      .then(response => response.text())
+      .then(json => {
+        this.setState({
+          mobiles: json
+        });
+        console.log(this.state.mobiles)
+      });
+  }
+
+
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <header className="App-header">
           <h1>Holaaa</h1>
         </header>
@@ -13,4 +34,3 @@ class App extends Component {
   }
 }
 
-export default App;
